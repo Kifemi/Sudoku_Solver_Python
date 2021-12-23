eae870b0a8701caee50e79ab5c2192eb103099aa
Kifemi <kifemi@outlook.com>
2021-12-23 17:31:29 +0200

    Solver creation in progress

--- solver.py
+++ solver.py
@@ -2,25 +2,140 @@ import settings
 
 
 def solve(puzzle):
-    possible_values = [[get_possible_values(puzzle, i, j) for j in range(9)] for i in range(9)]
-    return possible_values
+    temp_puzzle = puzzle
+
+    # Create a list of all possible values for each cell
+    # possible_values = []
+    # for i in range(9):
+    #     for j in range(9):
+    #         possible_values.append(get_possible_values(puzzle, i, j))
+    run = True
+
+    while run:
+        possible_values = [[get_possible_values(temp_puzzle, i, j) for j in range(9)] for i in range(9)]
+        # for value in possible_values:
+            # print(f"\t {value}")
+
+        # Fill unique values
+        if fill_unique(possible_values, temp_puzzle):
+            continue
+
+        # Check if possible value appears once in a row
+        if fill_row(possible_values, temp_puzzle):
+            continue
+
+        if fill_col(possible_values, temp_puzzle):
+            continue
+
+        run = False
+
+    return temp_puzzle
+
+
+def fill_unique(possible_values, temp_puzzle):
+    for i in range(9):
+        for j in range(9):
+            if possible_values[i][j] is not None:
+                value_set = possible_values[i][j]
+                # No possible values, impossible puzzle
+                if len(value_set) == 0:
+                    print("Impossible puzzle")
+                    return None
+                # If only one possible value in cell, fill it
+                if len(value_set) == 1:
+                    temp_puzzle[i][j] = next(iter(value_set))
+                    print(f"first loop: {i}, {j}, {temp_puzzle[i][j]}")
+                    return True
+    return False
+
+
+def fill_row(possible_values, temp_puzzle):
+    for i in range(9):
+        # Check if a possible value appears only once in a row
+        for j in range(9):
+            if possible_values[i][j] is not None:
+                value_set = possible_values[i][j]
+                for index, row_set in enumerate(possible_values[i]):
+                    if row_set is not None and index != j:
+                        value_set = value_set.difference(row_set)
+                # print(value_set, i, j)
+                if len(value_set) == 1:
+                    temp_puzzle[i][j] = next(iter(value_set))
+                    print(f"row loop: {i}, {j}, {temp_puzzle[i][j]}")
+                    # row_value_filled = True
+                    return True
+    return False
+
+
+def fill_col(possible_values, temp_puzzle):
+    for i in range(9):
+        for j in range(9):
+            value_set = [possible_values[j][i] for j in range(9)]
+            # print(f"\t\t {value_set}")
+            if value_set[j] is not None:
+                for index, col_set in enumerate(value_set):
+                    if col_set is not None and value_set[j] is not None and index != j:
+                        value_set[j] = value_set[j].difference(col_set)
+                if len(value_set[j]) == 1:
+                    temp_puzzle[j][i] = next(iter(value_set[j]))
+                    print(f"col loop: {j}, {i}, {temp_puzzle[j][i]}")
+                    return True
+    return False
+
 
 
 def get_possible_values(puzzle, row, col):
-    number_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-    # Check rows
-    for j in range(9):
-        number_set.discard(puzzle[row][j])
-    # Check columns
+    if not puzzle[row][col]:
+        number_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        # Check rows
+        for j in range(9):
+            number_set.discard(puzzle[row][j])
+        # Check columns
+        for i in range(9):
+            number_set.discard(puzzle[i][col])
+        # Check 3x3 square
+        x = row // 3
+        y = col // 3
+        for i in range(3*x, 3*x + 3):
+            for j in range(3*y, 3*y + 3):
+                number_set.discard(puzzle[i][j])
+        return number_set
+    return None
+
+
+def check_solution(puzzle):
+    numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    is_correct = True
     for i in range(9):
-        number_set.discard(puzzle[i][col])
-    # Check 3x3 square
-    x = row // 3
-    y = col // 3
-    for i in range(3*x, 3*x + 3):
-        for j in range(3*y, 3*y + 3):
-            number_set.discard(puzzle[i][j])
-    return list(number_set)
+        row = set(puzzle[i])
+        col = set()
+        if row != numbers:
+            is_correct = False
+            break
+        for j in range(9):
+            col.add(puzzle[j][i])
+        if col != numbers:
+            is_correct = False
+            break
+        square = set()
+        for j in range(9):
+            x = i // 3
+            y = j // 3
+            for i2 in range(3 * x, 3 * x + 3):
+                for j2 in range(3 * y, 3 * y + 3):
+                    square.add(puzzle[i2][j2])
+        if square != numbers:
+            is_correct = False
+            break
+    return is_correct
 
 
-print(solve(settings.puzzle))
+# print(fill_col(None, settings.puzzle))
+for row in solve(settings.puzzle_easy):
+    print(row)
+
+print(check_solution(solve(settings.puzzle_easy)))
+# print(solve(settings.puzzle_easy))
+# print(settings.puzzle)
+# print(solve(settings.puzzle))
+# print(settings.puzzle)
